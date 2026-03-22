094b1b4852a544d6d3d0aabc7a0c5fd51547058a
Subhajit Choudhury <122729012+Subhajit-FD@users.noreply.github.com>
2026-03-22 20:27:06 +0530

    db cached

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,17 +1,36 @@
 const mongoose = require("mongoose")
 
+let cached = global.mongoose
 
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
 
 async function connectToDB() {
+    if (cached.conn) {
+        return cached.conn
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        }
+
+        cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+            return mongoose
+        })
+    }
 
     try {
-        await mongoose.connect(process.env.MONGO_URI)
-
+        cached.conn = await cached.promise
         console.log("Connected to Database")
-    }
-    catch (err) {
+    } catch (err) {
+        cached.promise = null
         console.log(err)
+        throw err
     }
+
+    return cached.conn
 }
 
 module.exports = connectToDB
